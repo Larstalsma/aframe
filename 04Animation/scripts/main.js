c81b986e2507354fc135d7d98ad02cafbcf120f7
Larstalsma <47833022+Larstalsma@users.noreply.github.com>
2020-06-14 13:00:17 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,14 +1,20 @@
 var rotationSpeed = 0.01;
 var positionSpeed = 0.01;
-var scale = 0.01;
+var scaleSpeed = 0.01;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
 	myOtherBox.object3D.rotation.x -= rotationSpeed;
 	console.log(myOtherBox.object3D.rotation.x);
-	myOtherBox.object3D.rotation.y -= rotationSpeed;
-	console.log(myOtherBox.object3D.positionSpeed.y);
-	myOtherBox.object3D.rotation.z -= rotationSpeed;
+	myOtherBox.object3D.rotation.y -= positionSpeed;
+	console.log(myOtherBox.object3D.position.y);
+	myOtherBox.object3D.rotation.z -= scale.z;
+	console.log(myOtherBox.object3D.scale.z);
+	myOtherBox.object3D.rotation.x += rotationSpeed;
+	console.log(myOtherBox.object3D.rotation.x);
+	myOtherBox.object3D.rotation.y += positionSpeed;
+	console.log(myOtherBox.object3D.position.y);
+	myOtherBox.object3D.rotation.z += scaleSpeed;
 	console.log(myOtherBox.object3D.scale.z);
 }
 
